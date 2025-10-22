6bb542d381306e06c6b746bd3f368322ce107c17
David Elmo Ross <david.elmo.ross@icloud.com>
2025-10-22 14:11:09 -0400

    Second Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
-#include <Arduino.h>
+/*
+This program should count from 0-4 on the top line of the
+LCD continuously every 2 seconds on a green background
 
-// put function declarations here:
-int myFunction(int, int);
+What one statement must you add to this code to make this happen
+*/
+#include <Arduino.h> 
+#include <Wire.h>
+#include "rgb_lcd.h"
+#include <Ticker.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Ticker Tom;
+
+short counter=0;
+char buf[10];
+rgb_lcd LCD;
+
+void count(void)
+{
+    counter++;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+
+}
+void loop()
+{
+                                   // YOUR STATEMENT goes on THIS LINE
+    LCD.begin(16,2);
+    LCD.clear();
+    LCD.setRGB(0x00,0xff,0);
+    for(;;)
+    {
+        while(counter <=4)
+        {
+          LCD.setCursor(7,0);
+          sprintf(buf,"%d",counter);
+          LCD.print(buf);
+        }
+        counter=0;
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
